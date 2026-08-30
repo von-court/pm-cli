1941db453304d55009d3c7fdd12bf19a13ca245b
Brian Scott <191290+bscott@users.noreply.github.com>
2026-08-29 19:33:02 -0700

    [verified] chore(release): stamp 0.2.7 (#31)

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -5,7 +5,7 @@ import (
 	"github.com/bscott/pm-cli/internal/output"
 )
 
-var Version = "0.2.6"
+var Version = "0.2.7"
 
 type Globals struct {
 	JSON     bool   `help:"Output as JSON" name:"json"`
